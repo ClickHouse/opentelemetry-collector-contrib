b37b5f0771444ca53ec7382c7ff9ed975d9a36ec
Rory Crispin <rorycr@icloud.com>
2023-11-27 17:23:44 +0000

    [ClickHouse org] make creating db and tables optional

--- exporter/clickhouseexporter/factory.go
+++ exporter/clickhouseexporter/factory.go
@@ -34,15 +34,16 @@ func createDefaultConfig() component.Config {
 	queueSettings.NumConsumers = 1
 
 	return &Config{
-		TimeoutSettings:  exporterhelper.NewDefaultTimeoutSettings(),
-		QueueSettings:    queueSettings,
+		TimeoutSettings:   exporterhelper.NewDefaultTimeoutSettings(),
+		QueueSettings:     queueSettings,
 		BackOffConfig:    configretry.NewDefaultBackOffConfig(),
-		ConnectionParams: map[string]string{},
-		Database:         defaultDatabase,
-		LogsTableName:    "otel_logs",
-		TracesTableName:  "otel_traces",
-		MetricsTableName: "otel_metrics",
-		TTL:              0,
+		ConnectionParams:  map[string]string{},
+		Database:          defaultDatabase,
+		CreateDBAndTables: true, // this isn't actually default.
+		LogsTableName:     "otel_logs",
+		TracesTableName:   "otel_traces",
+		MetricsTableName:  "otel_metrics",
+		TTL:               0,
 	}
 }
 
