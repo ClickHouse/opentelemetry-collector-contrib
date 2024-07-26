948e90ec480a2fee3811e60e496dd408df245261
Tommy Li <tommy.li@clickhouse.com>
2024-07-26 08:09:32 -0700

    rename component type and regenerate metadata

--- processor/loghouseprocessor/generated_component_test.go
+++ processor/loghouseprocessor/generated_component_test.go
@@ -21,7 +21,7 @@ import (
 )
 
 func TestComponentFactoryType(t *testing.T) {
-	require.Equal(t, "processor", NewFactory().Type().String())
+	require.Equal(t, "loghouse", NewFactory().Type().String())
 }
 
 func TestComponentConfigStruct(t *testing.T) {
