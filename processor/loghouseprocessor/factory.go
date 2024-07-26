4f6d747e83edc1b47908610975bb61ec667a0038
Rory Crispin <Rory.crispin@clickhouse.com>
2024-07-26 12:25:31 +0100

    Correctly set loghouse processor name (#323)
    * make checks
    
    * Fix name of loghouse processor

--- processor/loghouseprocessor/factory.go
+++ processor/loghouseprocessor/factory.go
@@ -21,7 +21,7 @@ var (
 // NewFactory creates a factory for the routing processor.
 func NewFactory() processor.Factory {
 	return processor.NewFactory(
-		metadata.Type,
+		component.MustNewType("loghouse"),
 		createDefaultConfig,
 		processor.WithLogs(createLogsProcessor, metadata.LogsStability),
 	)
